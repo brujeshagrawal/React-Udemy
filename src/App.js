282fb63a20f5af083dd4a175495ba7e29b7b0d0b
brujeshagrawal <brujesha@mindfiresolutions.com>
2020-03-24 17:38:00 +0530

    added demo error handling to the person component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import classes from "./App.css";
 
 import Person from "./Person/Person";
+import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
 
 class App extends Component {
   state = {
@@ -39,34 +40,27 @@ class App extends Component {
   };
 
   render() {
-    const style = {
-      backgroundColor: "green",
-      color: "white",
-      font: "inherit",
-      border: "1px solid blue",
-      padding: "8px",
-      cursor: "pointer"
-    };
-
     let persons = null;
+    let btnClass = "";
+
     if (this.state.showPersons) {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
             return (
-              <Person
-                name={person.name}
-                age={person.age}
-                click={() => this.deletePersonHandler(index)}
-                key={person.id}
-                changed={event => this.nameChangedHandler(event, person.id)}
-              />
+              <ErrorBoundary key={person.id}>
+                <Person
+                  name={person.name}
+                  age={person.age}
+                  click={() => this.deletePersonHandler(index)}
+                  changed={event => this.nameChangedHandler(event, person.id)}
+                />
+              </ErrorBoundary>
             );
           })}
         </div>
       );
-
-      style.backgroundColor = "red";
+      btnClass = classes.Red;
     }
 
     const assignedClasses = [];
@@ -81,7 +75,7 @@ class App extends Component {
       <div className={classes.App}>
         <h1>Hi. I am a React App</h1>
         <p className={assignedClasses.join(" ")}>This is really working!</p>
-        <button style={style} onClick={this.togglePersonHandler}>
+        <button className={btnClass} onClick={this.togglePersonHandler}>
           Toggle Persons
         </button>
         {persons}
